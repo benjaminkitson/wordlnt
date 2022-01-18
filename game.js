ea64249fc7120abb58260b2a539fbf88049e3ed7
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-01-18 13:18:40 +0000

    clicking dummy 'try' key shifts guess to next row when enough letters are added

--- game.js
+++ game.js
@@ -1,32 +1,33 @@
 const keyboard = document.querySelector('.keyboard');
-const firstGuess = document.querySelector('.first-guess');
-const secondGuess = document.querySelector('.second-guess');
-const thirdGuess = document.querySelector('.third-guess');
-const fourthGuess = document.querySelector('.fourth-guess');
-const fifthGuess = document.querySelector('.fifth-guess');
-const sixthGuess = document.querySelector('.sixth-guess');
+const guesses = document.querySelectorAll('.guess')
 
 let guessArray = []
+let guessCount = 0
 
 keyboard.addEventListener('mouseup', (e) => {
-  if (e.target.getAttribute("data-key") === 'pop') {
+  if (e.target.getAttribute('data-key') === 'try') {
+    if (guessArray.length === 5) {
+      guessCount++
+      guessArray = []
+    }
+  } else if (e.target.getAttribute('data-key') === 'pop') {
     guessArray.pop()
-    const guess = Array.from(firstGuess.children)
+    const guess = Array.from(guesses[guessCount].children)
     guess.forEach((cell, i) => {
       if (guessArray[i]) {
         cell.innerHTML = guessArray[i]
       } else {
-        cell.innerHTML = ""
+        cell.innerHTML = ''
       }
     })
-  } else if (guessArray.length <= 4 && e.target.getAttribute("data-key")) {
-    guessArray.push(e.target.getAttribute("data-key"))
-    const guess = Array.from(firstGuess.children)
+  } else if (guessArray.length <= 4 && e.target.getAttribute('data-key')) {
+    guessArray.push(e.target.getAttribute('data-key'))
+    const guess = Array.from(guesses[guessCount].children)
     guess.forEach((cell, i) => {
       if (guessArray[i]) {
         cell.innerHTML = guessArray[i]
       } else {
-        cell.innerHTML = ""
+        cell.innerHTML = ''
       }
     })
   }

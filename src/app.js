149f5a67c3c7fde854417054e44c8897bac22678
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-02-02 16:33:44 +0000

    app.js more or less set up

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const request = require('postman-request');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '../templates/views'));
+hbs.registerPartials(path.join(__dirname, '../templates/partials'));
+
+app.use(express.static(
+  path.join(__dirname, "../public")
+))
+
+app.get('', (req, res) => {
+  res.render('index.hbs')
+})
+
+app.listen(PORT, () => {
+  console.log("It has begun!")
+})

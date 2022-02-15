aebca49f7650f8cf06f5e8f2626627a2fd8956da
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-02-15 10:52:51 +0000

    forgot JSON.stringify

--- public/game.js
+++ public/game.js
@@ -40,7 +40,7 @@ if (!gameData.isCompleted) gameData.isCompleted = JSON.stringify(false)
 if (!gameData.inProgress) gameData.inProgress = JSON.stringify(false)
 if (!gameData.turns) gameData.turns = JSON.stringify([])
 if (!gameData.currentWord) {
-  gameData.currentWord = 'PENDING'
+  gameData.currentWord = JSON.stringify('')
   getWord()
 } else {
   wArray = JSON.parse(gameData.currentWord).split('')

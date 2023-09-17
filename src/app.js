b8ad66a5cbe4880a2ab837e6c315b689f3d6ec57
Benjamin Kitson <benjaminkitson13@gmail.com>
2023-09-17 14:26:45 +0100

    some tweaks

--- src/app.js
+++ src/app.js
@@ -1,53 +1,51 @@
-const path = require('path');
-const express = require('express');
-const hbs = require('hbs');
-const request = require('postman-request');
-const wordGen = require('./word-gen.js')
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const request = require("postman-request");
+const wordGen = require("./word-gen.js");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, '../templates/views'));
-hbs.registerPartials(path.join(__dirname, '../templates/partials'));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "../templates/views"));
+hbs.registerPartials(path.join(__dirname, "../templates/partials"));
 
-app.use(express.static(
-  path.join(__dirname, "../public")
-));
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.get('', (req, res) => {
-  res.render('index.hbs')
+app.get("", (_req, res) => {
+  res.render("index.hbs");
 });
 
-let epoch
-let newWord
+let epoch;
+let newWord;
 
-app.get('/whywouldyouevencheatatthisgame', (req, res) => {
-  const word = newWord
+app.get("/whywouldyouevencheatatthisgame", (_req, res) => {
+  const word = newWord;
   res.send({
     word,
     epoch,
-    currentTime: Date.now()
-  })
+    currentTime: Date.now(),
+  });
 });
 
-app.get('/thumbnailimageforwebsitepreviews', (req, res) => {
-  res.sendFile(path.join(__dirname, "../media/wordlnt.png"))
-})
+app.get("/thumbnailimageforwebsitepreviews", (_req, res) => {
+  res.sendFile(path.join(__dirname, "../media/wordlnt.png"));
+});
 
 app.listen(PORT, () => {
-  epoch = Date.now() + (10800000 - (Date.now() % 10800000))
-  let now = new Date(Date.now()).getHours()
-  let hour
-  if (!newWord) newWord = wordGen()
+  epoch = Date.now() + (10800000 - (Date.now() % 10800000));
+  let now = new Date(Date.now()).getHours();
+  let hour;
+  if (!newWord) newWord = wordGen();
   setInterval(() => {
-    now = new Date(Date.now()).getHours()
+    now = new Date(Date.now()).getHours();
     if (now % 3 === 0 && now != hour && hour) {
-      epoch += 10800000
-      newWord = wordGen()
-      hour = now
+      epoch += 10800000;
+      newWord = wordGen();
+      hour = now;
     }
   }, 100);
-  hour = now
+  hour = now;
 });
